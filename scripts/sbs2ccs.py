6919048a43aed5ef9cfda7bd60b31cdd9be128f8
sjin09 <sl17@sanger.ac.uk>
2022-10-04 16:16:56 +0100

    update sbs2ccs.py

--- scripts/sbs2ccs.py
+++ scripts/sbs2ccs.py
@@ -2,13 +2,12 @@
 
 import re
 import sys
-import time
 import pysam
 import cyvcf2
+import natsort
 import argparse
-import multiprocessing as mp
+from typing import List, Tuple
 from collections import defaultdict
-from typing import Dict, List, Tuple
 
 
 def parse_args(args):
@@ -20,7 +19,7 @@ def parse_args(args):
         "--bam",
         type=str,
         required=True,
-        help="BAM filed to read",
+        help="BAM file to read",
     )
     parser.add_argument(
         "--vcf",
@@ -29,16 +28,11 @@ def parse_args(args):
         help="single base substitution VCF file to read",
     )
     parser.add_argument(
-        "--region",
-        type=str,
+        "--min_bq",
+        type=int,
+        default=93,
         required=False,
-        help="target chromosome",
-    )
-    parser.add_argument(
-        "--region_list",
-        type=str,
-        required=False,
-        help="list of target chromosomes separated by new line"
+        help="minimum base quality score",
     )
     parser.add_argument(
         "--min_mapq",
@@ -48,18 +42,11 @@ def parse_args(args):
         help="minimum mapping quality score",
     )
     parser.add_argument(
-        "--threads",
-        type=int,
-        default=1,
-        required=False,
-        help="number of threads"
-    )
-    parser.add_argument(
         "-o",
         "--out",
         type=str,
         required=True,
-        help="file to return",
+        help="file to write",
     )
     args = args[1:]
     return parser.parse_args(args)
@@ -73,13 +60,14 @@ class BAM:
         self.tend = line.reference_end
         self.target_alignment_length = self.tend - self.tstart
         # query
-        self.qname = line.query_name
+        self.qname = line.query_name.replace("/ccs", "")
         self.qstart = line.query_alignment_start
         self.qend = line.query_alignment_end
         self.qseq = line.query_sequence
         self.qlen = len(self.qseq)
         self.mapq = line.mapping_quality
         self.bq_int_lst = line.query_qualities
+        self.qv = sum(self.bq_int_lst)/self.qlen
         self.query_alignment_length = self.qend - self.qstart
         self.query_alignment_proportion = self.query_alignment_length/float(self.qlen)
         self.cs_tag = line.get_tag("cs") if line.has_tag("cs") else "."
@@ -91,10 +79,12 @@ class BAM:
         else:
             self.is_primary = False
 
+
     def cs2lst(self):
         cslst = [cs for cs in re.split("(:[0-9]+|\*[a-z][a-z]|[=\+\-][A-Za-z]+)", self.cs_tag)]
         cslst = [cs.upper() for cs in cslst if cs != ""]
         return cslst
+
             
     def cs2tuple(self) -> List[Tuple[int, str, str, int, int]]:
         qpos = self.qstart
@@ -128,6 +118,7 @@ class BAM:
             self.cstuple_lst.append(t)
             qpos += mlen
 
+
     def cs2subindel(self):
         self.cs2tuple()
         tpos = self.tstart
@@ -149,6 +140,7 @@ class BAM:
             tpos += ref_len 
             qpos += alt_len
 
+
 class VCF:
     def __init__(self, line):
         arr = line.strip().split()
@@ -160,42 +152,11 @@ class VCF:
         self.qual = arr[5]
         self.qual = float(self.qual) if self.qual != "." else self.qual
         self.is_pass = True if arr[6] == "PASS" else False
-        self.info = arr[7]
-        self.format = arr[8]
-        self.sample_format = arr[9]
-        self.sample_format_lst = self.sample_format.split(":")
-        self.sample_gt = self.sample_format_lst[0]
-        self.is_snp = False
-        self.is_indel = False
         if len(self.alt_lst) == 1:  # bi-allelic
             self.is_biallelic = True
             self.alt = self.alt_lst[0]
-            if len(self.ref) == 1 and len(self.alt) == 1:  # snp
-                self.is_snp = True
-            else:  # indel
-                self.is_indel = True
         else:
             self.is_biallelic = False
-
-
-def load_chrom_lst(
-    region: str, 
-    region_file: str, 
-) -> Tuple[List[str], List[Tuple[str, int, int]]]:
-
-    chrom_lst = []
-    if region is None and region_file is not None:
-        for line in open(region_file).readlines():
-            chrom_lst.append(line.strip().split()[0])
-    elif region is not None and region_file is None:
-        chrom_lst = [region]
-    elif region is not None and region_file is not None:
-        for line in open(region_file).readlines():
-            chrom_lst.append(line.strip().split()[0])
-    else:
-        print("--region or --region_list parameter is required")
-        sys.exit()
-    return chrom_lst
 
 
 def load_vcf_file(vcf_file: str):
@@ -216,66 +177,47 @@ def load_vcf_file(vcf_file: str):
     return chrom2sbs_lst
 
 
-def get_ccs(
-    chrom: str,
-    sbs_lst: List[Tuple[int, int, str]],
-    bam_file: str,
-    min_mapq: int,
-    chrom2sbs2ccs: Dict[str, Dict[str, Tuple[str, int, int, int, int]]]
-):
-    sbs2ccs = {}
-    alignments = pysam.AlignmentFile(bam_file, "rb")
-    for sbs in sbs_lst:
-        pos = sbs[0]
-        for line in alignments.fetch(chrom, pos, pos + 1):
-            ccs = BAM(line)
-            if ccs.mapq >= min_mapq and ccs.is_primary:
-                ccs.cs2subindel()
-                tsbs_set = set(ccs.tsbs_lst)
-                if sbs in tsbs_set:
-                    sbs2ccs[sbs] = [ccs.qname, ccs.qlen, ccs.mapq, len(ccs.tsbs_lst), len(ccs.tindel_lst)]
-    chrom2sbs2ccs[chrom] = sbs2ccs
-
 def sbs2ccs(
     bam_file: str, 
     vcf_file: str,
-    region: str,
-    region_file: str,
+    min_bq: int,
     min_mapq: int,
-    threads: int,
     out_file: str
 ): 
 
-    chrom_lst = load_chrom_lst(region, region_file)
-    chrom2sbs_lst = load_vcf_file(vcf_file)
-    p = mp.Pool(threads)
-    manager = mp.Manager()
-    chrom2sbs2ccs = manager.dict()
-    get_ccs_arg_lst = [
-        (
-            chrom,
-            chrom2sbs_lst[chrom],
-            bam_file,
-            min_mapq,
-            chrom2sbs2ccs,
-        )
-        for chrom in chrom_lst
-    ]
-    print("sbs2ccs started retrieving CCS read for each single base substitution")
-    p.starmap(
-        get_ccs, get_ccs_arg_lst,
-    )
-    p.close()
-    p.join()
-    print("sbs2ccs finished retrieving CCS read for each single base substitution")
-   
-    # def dump_sbs2ccs()
     o = open(out_file, "w") 
-    o.write("{}\n".format("\t".join(["sbs", "ccs", "ccs_len", "mapq", "sbs_cnt", "indel_cnt", "cell", "zmw"])))
-    for chrom in chrom2sbs2ccs:
-        for (pos, ref, alt), (qname, qlen, mapq, tsbs_cnt, tindel_cnt) in chrom2sbs2ccs[chrom].items():
-            cell, zmw, _ = qname.split("/")
-            o.write("{}:{}_{}/{}\t{}\t{}\t60\t{}\t{}\t{}\t{}\n".format(chrom, pos, ref, alt, qname, qlen, tsbs_cnt, tindel_cnt, cell, zmw))
+    chrom2sbs_lst = load_vcf_file(vcf_file)
+    alignments = pysam.AlignmentFile(bam_file, "rb")
+    chrom_lst = natsort.natsorted(list(chrom2sbs_lst.keys()))
+    o.write("{}\n".format("\t".join(["sbs", "qname", "qv", "qlen", "mapq", "sbs_count", "bq93_sbs_count", "indel_count"])))
+    for chrom in chrom_lst:
+        for sbs in chrom2sbs_lst[chrom]:
+            pos, ref, alt = sbs
+            for line in alignments.fetch(chrom, pos, pos + 1):
+                ccs = BAM(line)
+                if not ccs.is_primary:
+                    continue
+                
+                if ccs.mapq >= min_mapq:
+                    ccs.cs2subindel()
+                    tsbs_set = set(ccs.tsbs_lst)
+                    if sbs in tsbs_set:
+                        passed_tsbs_lst = [tsbs for tsbs, qbq in zip(ccs.tsbs_lst, ccs.qsbs_bq_lst) if qbq >= min_bq]
+                        o.write("{}:{}_{}/{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(
+                            chrom, 
+                            pos, 
+                            ref, 
+                            alt, 
+                            ccs.qname, 
+                            ccs.qv, 
+                            ccs.qlen, 
+                            ccs.mapq, 
+                            len(ccs.tsbs_lst),
+                            len(passed_tsbs_lst),
+                            len(ccs.tindel_lst)
+                            )
+                        )
+                        break
     o.close() 
 
 
@@ -284,10 +226,8 @@ def main():
     sbs2ccs(
         options.bam, 
         options.vcf, 
-        options.region, 
-        options.region_list, 
+        options.min_bq,
         options.min_mapq, 
-        options.threads,
         options.out
     )
     sys.exit(0)
