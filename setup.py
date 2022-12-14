e84bb3ca4696cc2a63dee2a5e55b3ed6a56f8a01
sjin09 <sl17@sanger.ac.uk>
2022-12-14 17:57:53 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,29 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
-setup()
+setup(
+        name="himut", 
+        version='1.0.0',
+        project_urls={
+            "homepage": "https://github.com/sjin09/himut",
+            "repository": "https://github.com/sjin09/himut"
+        },
+        author='Sangjin Lee',
+        author_email='sl17@sanger.ac.uk',
+        license='MIT',
+        classifiers=[
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9'
+        ],
+        entry_points={"console_scripts": ["himut = himut.__main__:main"]},
+        packages=['himut'],
+        package_dir={"": "src"},
+        package_data={"himut": ["*.typed"]},
+        install_requires=[
+            'argparse==1.*,>=1.4.0', 'biopython==1.*,>=1.78.0',
+            'click==7.*,>=7.0.0', 'natsort==8.*,>=8.0.0', 'numpy==1.*,>=1.20.2',
+            'psutil==5.*,>=5.8.0', 'pysam==0.*,>=0.16.0'
+        ],
+)
