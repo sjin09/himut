0d4e361d554fe235e693358a38a043b881597423
sjin09 <sl17@sanger.ac.uk>
2023-04-06 16:34:54 +0100

    edit som2sbs52

--- scripts/som2sbs52.py
+++ scripts/som2sbs52.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 import sys
+import natsort
 import argparse
 from collections import defaultdict 
-from himut.mutlib import sbs48_lst, sbs96_to_sbs48, sbs96_to_tri, sbs96_to_sub
+from himut.mutlib import sbs52_lst, sbs96_to_sbs52, sbs96_to_tri, sbs96_to_sub
 
 
 def parse_args(args):
@@ -15,50 +16,64 @@ def parse_args(args):
         "--input",
         type=str,
         required=True,
-        help="tsv file to read SBS96 counts"
+        help="file to read SBS96 counts"
+    )
+    parser.add_argument(
+        "--tri",
+        type=str,
+        required=True,
+        help="file to return SBS52 reference trinucleotide sequence contexts"
     )
     parser.add_argument(
         "-o",
         "--output",
         type=str,
         required=True,
-        help="file to return SBS48 counts"
+        help="file to return SBS52 counts"
     )
     args = args[1:]
     return parser.parse_args(args)
 
 
-def load_sbs48_counts(tsv_file: str):
+def load_sbs52_counts(tsv_file: str):
 
-    sbs48_counts = defaultdict(lambda: 0)
-    sbs96_counts = defaultdict(lambda: 0)
+    sbs52_counts = defaultdict(lambda: 0)
+    sbs52_tri_counts = defaultdict(lambda: 0)
     for line in open(tsv_file).readlines():
         if line.startswith("#"):
             continue
         elif line.startswith("sub"):
             continue
-        _, _, sbs96, _, normcounts, _, _, _, _, _ = line.strip().split()
-        sbs48 = sbs96_to_sbs48[sbs96]
-        sbs48_counts[sbs48] += float(normcounts)
-        sbs96_counts[sbs96] = normcounts
-    return sbs48_counts
+        _, _, sbs96, _, normcounts, _, _, ref_tri_count, _, _ = line.strip().split()
+        sbs52 = sbs96_to_sbs52[sbs96]
+        tri = sbs96_to_tri[sbs52]
+        sbs52_counts[sbs52] += float(normcounts)
+        sbs52_tri_counts[tri] += int(ref_tri_count)
+    return sbs52_counts, sbs52_tri_counts
 
 
-def dump_sbs48_counts(infile, outfile):
+def dump_sbs52_counts(infile, trifile, outfile):
 
     o = open(outfile, "w")
-    sbs48_counts = load_sbs48_counts(infile)
+    sbs52_counts, sbs52_tri_counts = load_sbs52_counts(infile)
     o.write("sub\ttri\tsbs48\tcounts\tnormcounts\tref_tri_ratio\tref_ccs_tri_ratio\tref_tri_count\tref_callable_tri_count\tccs_callable_tri_count\n")
-    for sbs48 in sbs48_lst:
-        tri = sbs96_to_tri[sbs48]
-        sub = sbs96_to_sub[sbs48]
-        o.write("{}\t{}\t{}\t.\t{}\t.\t.\t.\t.\t.\n".format(sub, tri, sbs48, sbs48_counts[sbs48])) 
+    for sbs52 in sbs52_lst:
+        tri = sbs96_to_tri[sbs52]
+        sub = sbs96_to_sub[sbs52]
+        o.write("{}\t{}\t{}\t.\t{}\t.\t.\t.\t.\t.\n".format(sub, tri, sbs52, sbs52_counts[sbs52])) 
     o.close()
 
+    p = open(trifile, "w")
+    p.write("{}\t{}\n".format("tri", "count")) 
+    sbs52_tri_lst = natsort.natsorted(list(sbs52_tri_counts.keys()))
+    for tri in sbs52_tri_lst:
+        p.write("{}\t{}\n".format(tri, sbs52_tri_counts[tri]))
+    p.close()
+        
         
 def main():
     options = parse_args(sys.argv)
-    dump_sbs48_counts(options.input, options.output)
+    dump_sbs52_counts(options.input, options.tri, options.output)
     sys.exit(0)
 
 
