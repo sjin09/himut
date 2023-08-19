b7a82a752e411ba953c6cfb1c3496ac1af2a2b65
sjin09 <sl17@sanger.ac.uk>
2023-08-19 21:21:21 +0900

    update bam2ccs_statistics

--- scripts/bam2ccs_statistics.py
+++ scripts/bam2ccs_statistics.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 
-import re
 import sys
 import pysam
-import tabix
-import cyvcf2
 import natsort
 import argparse
+import numpy as np
+import himut.bamlib
 import multiprocessing as mp
-from collections import defaultdict
-from typing import Dict, List, Tuple, Set
+from typing import Dict, List, Tuple
 
 
 def parse_args(args):
@@ -36,19 +34,6 @@ def parse_args(args):
         help="list of target chromosomes separated by new line"
     ) 
     parser.add_argument(
-        "--vcf",
-        type=str,
-        required=True,
-        help="deepvariant VCF file to read",
-    )
-    parser.add_argument(
-        "--min_bq",
-        type=int,
-        default=93,
-        required=False,
-        help="minimum base quality score",
-    )
-    parser.add_argument(
         "-t",
         "--threads",
         type=int,
@@ -67,136 +52,6 @@ def parse_args(args):
     return parser.parse_args(args)
 
 
-class BAM:
-    def __init__(self, line):
-        # target
-        self.tname = line.reference_name
-        self.tstart = line.reference_start
-        self.tend = line.reference_end
-        self.tcoord = "{}:{}-{}".format(self.tname, self.tstart, self.tend)
-        # query
-        self.qname = line.query_name.replace("/ccs", "")
-        self.qstart = line.query_alignment_start
-        self.qend = line.query_alignment_end
-        self.qseq = line.query_sequence
-        self.qlen = len(self.qseq)
-        self.mapq = line.mapping_quality
-        self.bq_int_lst = line.query_qualities
-        self.qv = sum(self.bq_int_lst)/self.qlen
-        self.hbq_proportion = self.bq_int_lst.count(93)/float(self.qlen)
-        self.query_alignment_length = self.qend - self.qstart
-        self.query_alignment_proportion = self.query_alignment_length/float(self.qlen)
-        self.cs_tag = line.get_tag("cs") if line.has_tag("cs") else "."
-        if line.has_tag("tp"):
-            if line.get_tag("tp") == "P":
-                self.is_primary = True
-            else:
-                self.is_primary = False
-        else:
-            self.is_primary = False
-
-
-    def cs2lst(self):
-        cslst = [cs for cs in re.split("(:[0-9]+|\*[a-z][a-z]|[=\+\-][A-Za-z]+)", self.cs_tag)]
-        cslst = [cs.upper() for cs in cslst if cs != ""]
-        return cslst
-
-            
-    def cs2tuple(self) -> List[Tuple[int, str, str, int, int]]:
-        qpos = self.qstart
-        self.cstuple_lst = []
-        cs_lst = self.cs2lst()
-        for cs in cs_lst:
-            m = cs[1:]
-            mlen = len(m)
-            qstart = qpos
-            if cs.startswith("="):  # match # --cs=long
-                cs = ":{}".format(mlen)
-                t = (1, m, m, mlen, mlen)
-            elif cs.startswith(":"):  # match # --cs=short
-                mlen = int(m)
-                qend = qpos + mlen
-                m = self.qseq[qstart:qend]
-                t = (1, m, m, mlen, mlen)
-            elif cs.startswith("*"):  # snp # target and query
-                mlen = 1
-                ref, alt = list(m)
-                t = (2, ref, alt, 1, 1)
-            elif cs.startswith("+"):  # insertion # query
-                ref = self.qseq[qpos - 1]
-                alt = ref + m
-                t = (3, ref, alt, 0, mlen)
-            elif cs.startswith("-"):  # deletion # target
-                alt = self.qseq[qpos - 1]
-                ref = alt + m
-                t = (4, ref, alt, mlen, 0)
-                mlen = 0
-            self.cstuple_lst.append(t)
-            qpos += mlen
-
-
-    def cs2subindel(self):
-        self.cs2tuple()
-        tpos = self.tstart
-        qpos = self.qstart
-        self.tsbs_lst = []
-        self.qsbs_lst = []
-        self.qsbs_bq_lst = []
-        self.tindel_lst = []
-        for cstuple in self.cstuple_lst:
-            state, ref, alt, ref_len, alt_len, = cstuple
-            if state == 2:  # snp 
-                if ref == "N": 
-                    continue
-                self.qsbs_bq_lst.append(self.bq_int_lst[qpos])
-                self.tsbs_lst.append((tpos + 1, ref, alt))
-                self.qsbs_lst.append((qpos + 1, alt, ref))
-            elif state == 3 or state == 4:  # insertion
-                self.tindel_lst.append((tpos, ref, alt))
-            tpos += ref_len 
-            qpos += alt_len
-
-
-class VCF:
-    def __init__(self, line):
-        arr = line.strip().split()
-        self.chrom = arr[0]
-        self.pos = int(arr[1])
-        self.id = arr[2]
-        self.ref = arr[3]
-        self.alt_lst = arr[4].split(",")
-        self.qual = arr[5]
-        self.qual = float(self.qual) if self.qual != "." else self.qual
-        self.is_pass = True if arr[6] == "PASS" else False
-        self.info = arr[7]
-        self.format_lst = arr[8].split(":")
-        self.sample_format_lst = arr[9].split(":")
-        hsh = {i:j for i,j in zip(self.format_lst, self.sample_format_lst)}
-        if "GT" in hsh: self.sample_gt = hsh["GT"] 
-        if "PS" in hsh: self.sample_phase_set = hsh["PS"] 
-        if "AD" in hsh: 
-            arr = hsh["AD"].split(",")
-            self.ref_count = arr[0]
-            self.alt_count_arr = arr[1:]
-
-        self.is_snp = False
-        self.is_dbs = False
-        self.is_indel = False
-        if len(self.alt_lst) == 1:  # bi-allelic
-            self.is_biallelic = True
-            self.alt = self.alt_lst[0]
-            if len(self.ref) == 1 and len(self.alt) == 1:  # snp
-                self.is_snp = True
-            elif len(self.ref) == len(self.alt) == 2:
-                self.is_dbs = True
-            elif len(self.ref) > len(self.alt): # del
-                self.is_indel = True
-            elif len(self.ref) < len(self.alt): # ins
-                self.is_indel = True
-        else:
-            self.is_biallelic = False
-
-
 def load_chrom_lst(
     region: str, 
     region_file: str, 
@@ -212,8 +67,8 @@ def load_chrom_lst(
         for line in open(region_file).readlines():
             chrom_lst.append(line.strip().split()[0])
     else:
-        print("--region or --region_list parameter is required")
-        sys.exit()
+        print("--region or --region_list parameter has not been provided")
+        print("defaulting to all the contigs and chromosomes")
     return chrom_lst 
 
 
@@ -232,129 +87,68 @@ def get_tname2tsize(bam_file: str) -> Tuple[List[str], Dict[str, int]]:
     return tname2tsize
 
 
-def load_vcf_file(
-    chrom: str,
-    chrom_len: int,
-    vcf_file: str
-) -> Set[Tuple[int, str, str]]:
-
-    snp_set = set()
-    if vcf_file.endswith(".vcf"):
-        for line in open(vcf_file):
-            if line.startswith("#"):
-                continue
-            v = VCF(line)
-            if v.chrom != chrom:
-                continue
-            if v.is_pass:
-                if v.is_biallelic:
-                    if v.is_snp:
-                        snp_set.add((v.pos, v.ref, v.alt))
-                else:
-                    for alt in v.alt_lst:
-                        if len(v.ref) == 1 and len(alt) == 1:
-                            snp_set.add((v.pos, v.ref, alt))
-    elif vcf_file.endswith(".bgz"):
-        tb = tabix.open(vcf_file)
-        records = tb.query(chrom, 0, chrom_len)
-        for record in records:
-            v = VCF("\t".join(record))            
-            if v.is_snp and v.is_pass:
-                if v.is_biallelic:
-                    if v.is_snp:
-                        snp_set.add((v.pos, v.ref, v.alt))
-                else:
-                    for alt in v.alt_lst:
-                        if len(v.ref) == 1 and len(alt) == 1:
-                            snp_set.add((v.pos, v.ref, alt))
-    return snp_set
-
 def get_ccs_statistics(
     chrom: str,
-    chrom_len: int,
     bam_file: str,
-    vcf_file: str,
-    min_bq: int,
-    chrom2ccs_lst: Dict[str, str] 
+    chrom2qlen_lst: Dict[str, List[int]] 
 ):
-  
-    ccs_lst = [] 
+
+    qlen_lst = []
     alignments = pysam.AlignmentFile(bam_file, "rb")
-    deepvariant_set = load_vcf_file(chrom, chrom_len, vcf_file)
     for line in alignments.fetch(chrom):
-        ccs = BAM(line)
+        ccs = himut.bamlib.BAM(line)
         if not ccs.is_primary:
             continue
-        
-        if ccs.mapq > 0:
-            ccs.cs2subindel()
-            deepvariant_snp_lst = []                    
-            himut_candidate_lst = []
-            himut_q93_candidate_lst = []
-            for tsbs, qbq in zip(ccs.tsbs_lst, ccs.qsbs_bq_lst): 
-                if tsbs in deepvariant_set:
-                    deepvariant_snp_lst.append(tsbs)
-                else:
-                    himut_candidate_lst.append(tsbs)
-                    if qbq >= min_bq:
-                        himut_q93_candidate_lst.append(tsbs)
-            ccs_lst.append(
-                "\t".join(
-                    [
-                        ccs.qname, 
-                        ccs.tcoord, 
-                        str(ccs.qv), 
-                        str(ccs.qlen), 
-                        str(ccs.mapq), 
-                        str(ccs.hbq_proportion), 
-                        str(ccs.query_alignment_proportion), 
-                        str(len(himut_q93_candidate_lst)), 
-                        str(len(himut_candidate_lst)),
-                        str(len(deepvariant_snp_lst)),
-                        str(len(ccs.tsbs_lst))
-                    ]
-                )
-            )
-    chrom2ccs_lst[chrom] = ccs_lst
+        qlen_lst.append(ccs.qlen)
+    chrom2qlen_lst[chrom] = qlen_lst
+
 
 def dump_ccs_statistics(
     bam_file: str,
-    vcf_file: str, 
     region: str,
     region_file: str,
-    min_bq: int,
     threads: int,
     out_file: str
 ): 
     
     tname2tsize = get_tname2tsize(bam_file)
     chrom_lst = load_chrom_lst(region, region_file)
+    if len(chrom_lst) == 0:
+        tgt_lst = natsort.natsorted(tname2tsize.keys())
+    else:
+        tgt_lst = chrom_lst
 
     p = mp.Pool(threads)
     manager = mp.Manager()
-    chrom2ccs_lst = manager.dict()
+    chrom2qlen_lst = manager.dict()
     get_ccs_statistics_arg_lst = [
         (
-            chrom,
-            tname2tsize[chrom],
+            tgt,
             bam_file,
-            vcf_file,
-            min_bq,
-            chrom2ccs_lst
+            chrom2qlen_lst
         )
-        for chrom in chrom_lst
+        for tgt in tgt_lst
     ]
     p.starmap(
         get_ccs_statistics, get_ccs_statistics_arg_lst,
     )
     p.close()
     p.join()
-  
+ 
+    qsum = 0
+    genome_qlen_lst = []
     o = open(out_file, "w")
-    o.write("{}\n".format("\t".join(["qname", "tcoord", "qv", "qlen", "mapq", "hbq_proportion", "alignment_proportion", "himut_q93_candidate_count", "himut_candidate_count", "snp_count", "sbs_count"])))
-    for chrom in chrom_lst:
-        for p in chrom2ccs_lst[chrom]: 
-            o.write("{}\n".format(p))
+    tsum = sum([tname2tsize[tgt] for tgt in tgt_lst])
+    o.write("{}\n".format("\t".join(["ccs_base", "assembly_base", "coverage", "qlen_mean", "qlen_std"])))
+    for tgt in tgt_lst:
+        for p in chrom2qlen_lst[tgt]: 
+            chrom_qlen_lst = chrom2qlen_lst[tgt]
+            genome_qlen_lst.extend(chrom_qlen_lst)
+            qsum += sum(chrom_qlen_lst)
+    coverage = qsum/tsum
+    qlen_mean = np.mean(genome_qlen_lst)
+    qlen_std = np.std(genome_qlen_lst)
+    o.write("{}\t{}\t{}\t{}\t{}\n".format(qsum, tsum, coverage, qlen_mean, qlen_std))
     o.close() 
 
 
@@ -362,10 +156,8 @@ def main():
     options = parse_args(sys.argv)
     dump_ccs_statistics(
         options.bam, 
-        options.vcf,
         options.region, 
         options.region_list,
-        options.min_bq,
         options.threads, 
         options.out
     )
