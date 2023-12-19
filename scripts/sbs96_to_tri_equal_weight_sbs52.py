7418ac525baa70420ee073b264b5eda6c4fbefe6
sjin09 <sl17@sanger.ac.uk>
2023-12-19 19:31:15 +0900

    add sbs96_to_tri_equal_weight_sbs52.py

---
+++ scripts/sbs96_to_tri_equal_weight_sbs52.py
@@ -0,0 +1,352 @@
+import sys
+import natsort
+import argparse
+from collections import defaultdict
+
+
+SBS96_TO_SBS52 = {
+    "A[C>A]A": "T[T>G]T",
+    "T[T>G]T": "T[T>G]T",
+    "A[C>A]C": "A[C>A]C",
+    "G[T>G]T": "A[C>A]C",
+    "A[C>A]G": "C[T>G]T",
+    "C[T>G]T": "C[T>G]T",
+    "A[C>A]T": "A[C>A]T",
+    "A[T>G]T": "A[C>A]T",
+    "C[C>A]A": "C[C>A]A",
+    "T[T>G]G": "C[C>A]A",
+    "C[C>A]C": "C[C>A]C",
+    "G[T>G]G": "C[C>A]C",
+    "C[C>A]G": "C[C>A]G",
+    "C[T>G]G": "C[C>A]G",
+    "C[C>A]T": "C[C>A]T",
+    "A[T>G]G": "C[C>A]T",
+    "G[C>A]A": "T[T>G]C",
+    "T[T>G]C": "T[T>G]C",
+    "G[C>A]C": "G[C>A]C",
+    "G[T>G]C": "G[C>A]C",
+    "G[C>A]G": "C[T>G]C",
+    "C[T>G]C": "C[T>G]C",
+    "G[C>A]T": "A[T>G]C",
+    "A[T>G]C": "A[T>G]C",
+    "T[C>A]A": "T[C>A]A",
+    "T[T>G]A": "T[C>A]A",
+    "T[C>A]C": "T[C>A]C",
+    "G[T>G]A": "T[C>A]C",
+    "T[C>A]G": "C[T>G]A",
+    "C[T>G]A": "C[T>G]A",
+    "T[C>A]T": "T[C>A]T",
+    "A[T>G]A": "T[C>A]T",
+    "A[C>T]A": "A[C>T]A",
+    "A[T>C]A": "A[C>T]A",
+    "A[C>T]C": "A[C>T]C",
+    "A[T>C]C": "A[C>T]C",
+    "A[C>T]G": "A[C>T]G",
+    "A[T>C]G": "A[C>T]G",
+    "A[C>T]T": "A[C>T]T",
+    "A[T>C]T": "A[C>T]T",
+    "C[C>T]A": "C[C>T]A",
+    "C[T>C]A": "C[C>T]A",
+    "C[C>T]C": "C[C>T]C",
+    "C[T>C]C": "C[C>T]C",
+    "C[C>T]G": "C[C>T]G",
+    "C[T>C]G": "C[C>T]G",
+    "C[C>T]T": "C[C>T]T",
+    "C[T>C]T": "C[C>T]T",
+    "G[C>T]A": "G[C>T]A",
+    "G[T>C]A": "G[C>T]A",
+    "G[C>T]C": "G[C>T]C",
+    "G[T>C]C": "G[C>T]C",
+    "G[C>T]G": "G[C>T]G",
+    "G[T>C]G": "G[C>T]G",
+    "G[C>T]T": "G[C>T]T",
+    "G[T>C]T": "G[C>T]T",
+    "T[C>T]A": "T[C>T]A",
+    "T[T>C]A": "T[C>T]A",
+    "T[C>T]C": "T[C>T]C",
+    "T[T>C]C": "T[C>T]C",
+    "T[C>T]G": "T[C>T]G",
+    "T[T>C]G": "T[C>T]G",
+    "T[C>T]T": "T[C>T]T",
+    "T[T>C]T": "T[C>T]T",
+    "A[C>G]A": "T[C>G]T",
+    "T[C>G]T": "T[C>G]T",
+    "A[C>G]C": "A[C>G]C",
+    "G[C>G]T": "A[C>G]C",
+    "A[C>G]G": "C[C>G]T",
+    "C[C>G]T": "C[C>G]T",
+    "A[C>G]T": "A[C>G]T",
+    "C[C>G]A": "C[C>G]A",
+    "T[C>G]G": "C[C>G]A",
+    "C[C>G]C": "C[C>G]C",
+    "G[C>G]G": "C[C>G]C",
+    "C[C>G]G": "C[C>G]G",
+    "G[C>G]A": "T[C>G]C",
+    "T[C>G]C": "T[C>G]C",
+    "G[C>G]C": "G[C>G]C",
+    "T[C>G]A": "T[C>G]A",
+    "A[T>A]A": "T[T>A]T",
+    "T[T>A]T": "T[T>A]T",
+    "A[T>A]C": "A[T>A]C",
+    "G[T>A]T": "A[T>A]C",
+    "A[T>A]G": "C[T>A]T",
+    "C[T>A]T": "C[T>A]T",
+    "A[T>A]T": "A[T>A]T",
+    "C[T>A]A": "C[T>A]A",
+    "T[T>A]G": "C[T>A]A",
+    "C[T>A]C": "C[T>A]C",
+    "G[T>A]G": "C[T>A]C",
+    "C[T>A]G": "C[T>A]G",
+    "G[T>A]A": "T[T>A]C",
+    "T[T>A]C": "T[T>A]C",
+    "G[T>A]C": "G[T>A]C",
+    "T[T>A]A": "T[T>A]A",
+}
+
+SBS96_TO_TRI = {
+    "A[C>A]A": "ACA",
+    "A[C>A]T": "ACT",
+    "A[C>A]G": "ACG",
+    "A[C>A]C": "ACC",
+    "T[C>A]A": "TCA",
+    "T[C>A]T": "TCT",
+    "T[C>A]G": "TCG",
+    "T[C>A]C": "TCC",
+    "G[C>A]A": "GCA",
+    "G[C>A]T": "GCT",
+    "G[C>A]G": "GCG",
+    "G[C>A]C": "GCC",
+    "C[C>A]A": "CCA",
+    "C[C>A]T": "CCT",
+    "C[C>A]G": "CCG",
+    "C[C>A]C": "CCC",
+    "A[C>G]A": "ACA",
+    "A[C>G]T": "ACT",
+    "A[C>G]G": "ACG",
+    "A[C>G]C": "ACC",
+    "T[C>G]A": "TCA",
+    "T[C>G]T": "TCT",
+    "T[C>G]G": "TCG",
+    "T[C>G]C": "TCC",
+    "G[C>G]A": "GCA",
+    "G[C>G]T": "GCT",
+    "G[C>G]G": "GCG",
+    "G[C>G]C": "GCC",
+    "C[C>G]A": "CCA",
+    "C[C>G]T": "CCT",
+    "C[C>G]G": "CCG",
+    "C[C>G]C": "CCC",
+    "A[C>T]A": "ACA",
+    "A[C>T]T": "ACT",
+    "A[C>T]G": "ACG",
+    "A[C>T]C": "ACC",
+    "T[C>T]A": "TCA",
+    "T[C>T]T": "TCT",
+    "T[C>T]G": "TCG",
+    "T[C>T]C": "TCC",
+    "G[C>T]A": "GCA",
+    "G[C>T]T": "GCT",
+    "G[C>T]G": "GCG",
+    "G[C>T]C": "GCC",
+    "C[C>T]A": "CCA",
+    "C[C>T]T": "CCT",
+    "C[C>T]G": "CCG",
+    "C[C>T]C": "CCC",
+    "A[T>A]A": "ATA",
+    "A[T>A]T": "ATT",
+    "A[T>A]G": "ATG",
+    "A[T>A]C": "ATC",
+    "T[T>A]A": "TTA",
+    "T[T>A]T": "TTT",
+    "T[T>A]G": "TTG",
+    "T[T>A]C": "TTC",
+    "G[T>A]A": "GTA",
+    "G[T>A]T": "GTT",
+    "G[T>A]G": "GTG",
+    "G[T>A]C": "GTC",
+    "C[T>A]A": "CTA",
+    "C[T>A]T": "CTT",
+    "C[T>A]G": "CTG",
+    "C[T>A]C": "CTC",
+    "A[T>C]A": "ATA",
+    "A[T>C]T": "ATT",
+    "A[T>C]G": "ATG",
+    "A[T>C]C": "ATC",
+    "T[T>C]A": "TTA",
+    "T[T>C]T": "TTT",
+    "T[T>C]G": "TTG",
+    "T[T>C]C": "TTC",
+    "G[T>C]A": "GTA",
+    "G[T>C]T": "GTT",
+    "G[T>C]G": "GTG",
+    "G[T>C]C": "GTC",
+    "C[T>C]A": "CTA",
+    "C[T>C]T": "CTT",
+    "C[T>C]G": "CTG",
+    "C[T>C]C": "CTC",
+    "A[T>G]A": "ATA",
+    "A[T>G]T": "ATT",
+    "A[T>G]G": "ATG",
+    "A[T>G]C": "ATC",
+    "T[T>G]A": "TTA",
+    "T[T>G]T": "TTT",
+    "T[T>G]G": "TTG",
+    "T[T>G]C": "TTC",
+    "G[T>G]A": "GTA",
+    "G[T>G]T": "GTT",
+    "G[T>G]G": "GTG",
+    "G[T>G]C": "GTC",
+    "C[T>G]A": "CTA",
+    "C[T>G]T": "CTT",
+    "C[T>G]G": "CTG",
+    "C[T>G]C": "CTC",
+}
+
+SBS52_LST = natsort.natsorted(list(set([sbs52 for (_sbs96, sbs52) in SBS96_TO_SBS52.items()])))
+SBS52_TRI_LST = list(set([SBS96_TO_TRI[sbs52] for sbs52 in SBS52_LST]))
+SBS52_TRI_COUNT = len(SBS52_TRI_LST)
+SBS52_TRI_WEIGHT = 1/SBS52_TRI_COUNT
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        "-i",
+        "--sbs96",
+        type=str,
+        required=True,
+        help="file to read SBS96 normalised counts",
+    )
+    parser.add_argument(
+        "-o",
+        "--sbs52",
+        type=str,
+        required=True,
+        help="file to return normalised SBS52 counts",
+    )
+    args = args[1:]
+    return parser.parse_args(args)
+
+
+def load_sbs52_counts(sbs96_file):
+
+    sbs52_to_count = defaultdict(lambda: 0)
+    for line in open(sbs96_file):
+        if line.startswith("#"):
+            continue
+        elif line.startswith("sub"):
+            continue
+        (
+            _sub,
+            _tri,
+            sbs96,
+            _count,
+            normcount,
+            _ref_tri_ratio,
+            _ref_ccs_tri_ratio,
+            _ref_tri_count,
+            _ref_callable_tri_count,
+            _ccs_callable_tri_count,
+        ) = line.strip().split()
+        sbs52 = SBS96_TO_SBS52[sbs96]
+        sbs52_to_count[sbs52] += int(normcount)
+    return sbs52_to_count
+
+
+def load_tri_freq(sbs96_file):
+
+    tri_count = defaultdict(lambda: 0)
+    for line in open(sbs96_file):
+        if line.startswith("#"):
+            continue
+        elif line.startswith("sub"):
+            continue
+        (
+            _sub,
+            tri,
+            _sbs96,
+            _count,
+            _normcount,
+            _ref_tri_ratio,
+            _ref_ccs_tri_ratio,
+            ref_tri_count,
+            _ref_callable_tri_count,
+            _ccs_callable_tri_count,
+        ) = line.strip().split()
+        tri_count[tri] = int(ref_tri_count)
+    tri_sum = sum(tri_count.values())
+    tri_freq = {tri: tri_count/float(tri_sum) for tri, tri_count in tri_count.items()}
+    return tri_freq
+
+
+def dump_sbs52_tri_equal_weights(
+    sbs96_file: str,
+    sbs52_file: str,
+) -> None:
+
+    outfile = open(sbs52_file, "w")
+    tri_freq = load_tri_freq(sbs96_file)
+    with open(sbs52_file, "w") as outfile:
+        print(
+            "SUB",
+            "TRI",
+            "SBS52",
+            "WEIGHTED_COUNT",
+            "COUNT",
+            "WEIGHT",
+            sep="\t",
+            file=outfile
+        )
+       
+        sbs52_count = defaultdict(list)
+        for line in open(sbs96_file).readlines():
+            if line.startswith("#"):
+                continue
+            elif line.startswith("sub"):
+                continue
+            (
+            _sub,
+            tri,
+            sbs96,
+            _count,
+            normcount,
+            _ref_tri_ratio,
+            _ref_ccs_tri_ratio,
+            _ref_tri_count,
+            _ref_callable_tri_count,
+            _ccs_callable_tri_count,
+            ) = line.strip().split()
+            sbs52 = SBS96_TO_SBS52[sbs96]
+            ## tri_equal_weight_normcounts = float(normcount)/(tri_freq[tri]/SBS52_TRI_WEIGHT)
+            sbs52_count[sbs52].append(float(normcount))
+            # tri_equal_weight_normcounts     
+       
+        for sbs52 in SBS52_LST:
+            _ubase, _, ref, _, alt, _, _dbase = list(sbs52)
+            sub = f"{ref}>{alt}"
+            tri = SBS96_TO_TRI[sbs52]
+            normcount_lst = sbs52_count[sbs52]
+            tri_equal_weight = (tri_freq[tri]/SBS52_TRI_WEIGHT)
+            tri_equal_weight_normcount = sum(normcount_lst)/(tri_freq[tri]/SBS52_TRI_WEIGHT)
+            print(
+                sub,
+                SBS96_TO_TRI[sbs52],
+                sbs52,
+                tri_equal_weight_normcount,
+                ",".join([str(normcount) for normcount in normcount_lst]),
+                tri_equal_weight,
+                sep="\t",
+                file=outfile
+            )
+
+
+def main():
+    options = parse_args(sys.argv)
+    dump_sbs52_tri_equal_weights(options.sbs96, options.sbs52)
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
